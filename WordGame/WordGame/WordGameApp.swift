c465f8ee7035a04de0f0e385d8b1f47c067f427d
Reza Bina <tavakolibina@gmail.com>
2022-11-11 23:56:09 +0200

    Initialize the project

---
+++ WordGame/WordGame/WordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameApp.swift
+//  WordGame
+//
+//  Created by Reza Bina on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
